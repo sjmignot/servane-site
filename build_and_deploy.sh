70a641ad2fa54feb69b4eacc85ad1545609c8c56
SERVANE BRIAND <servane@Servane-iMac-2.local>
2020-07-23 14:47:20 -0700

    changes made to site

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,3 +1,6 @@
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 echo '############################'
 echo '#     SAVING CHANGES       #'
 echo '############################'
