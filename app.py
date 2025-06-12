486d4d90cfd5fb01322e288b31903ece37d1f8a6
sjmignot <sjmignot@gmail.com>
2025-06-11 22:36:56 -0700

    fix

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
         freezer.freeze()
     else:
-        app.run(host="0.0.0.0:5001", debug=True)
+        app.run(host="0.0.0.0", debug=True)
